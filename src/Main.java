05370c6a0e34ac370bebe59236cb4fc7406ad618
wcamer <xeus.xiii@hotmail.com>
2024-02-06 11:34:58 -0700

    Minor clean up

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ import java.util.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-     //System.out.println("Wellcome to Sorry Java!"); //error
     public static void main(String[] args) {
 
         System.out.println("Welcome to Sorry Java!\n");
